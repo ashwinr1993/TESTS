cc7de70114b38405b9100ab638933aad77212c46
ashwinr1993 <ashwinr1993@gmail.com>
2022-06-02 17:31:52 +0530

    added cf

--- ptp_preemp/test.sh
+++ ptp_preemp/test.sh
@@ -5,8 +5,8 @@ wr 0x4 0x0F28 0x0000
 wr 0x5 0x0F28 0x0000
 
 # Configure DA,SA,EtherType,TX/RX keys
-source TESTS/preemp/Dut_phytool.sh
-source TESTS/preemp/Lp_phytool.sh
+source TESTS/ptp_preemp/Dut_phytool.sh
+source TESTS/ptp_preemp/Lp_phytool.sh
 
 # Enable Frame Pre-emption on Egress and Ingress Sides
 wr 0x4 0x0F29 0x0121
