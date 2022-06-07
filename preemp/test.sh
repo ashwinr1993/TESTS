02762e5ef29a4838396e96ce19d3d8fe18af58d9
ashwinr1993 <ashwinr1993@gmail.com>
2022-06-07 18:05:16 +0530

    preemp

--- preemp/test.sh
+++ preemp/test.sh
@@ -7,5 +7,9 @@ source TESTS/common/macsec_enable.sh
 source TESTS/preemp/Dut_phytool.sh
 source TESTS/preemp/Lp_phytool.sh
 
+# mask DA/SA/Ethertype
+source TESTS/common/mask_da_sa.sh
+source TESTS/common/mask_etype.sh
+
 # Enable Frame Pre-emption on Egress and Ingress Sides
 source TESTS/common/enable_frame_preemption.sh
