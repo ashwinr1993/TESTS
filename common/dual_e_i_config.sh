90bcbb78a3c7e5c9c9a3fcf0634b86791d5d3042
ashwinr1993 <ashwinr1993@gmail.com>
2022-06-09 11:28:52 +0530

    added dual cfg

---
+++ common/dual_e_i_config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source TEST/common/macsec_enable.sh
+
+source TESTS/common/Dut1e_phytool.sh
+source TESTS/common/Lp2i_phytool.sh
+
+source TESTS/common/Dut2e_phytool.sh
+source TESTS/common/Lpii_phytool.sh
+
+source TESTS/common/mask_da_sa.sh
+source TESTS/common/mask_etype.sh
